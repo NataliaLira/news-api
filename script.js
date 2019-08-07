fad2d84cb25ec2340541bdfc634b97ba8f3f1b1d
NataliaLira <natalia.rlira@gmail.com>
2019-08-07 15:33:08 -0300

    getting the api json

---
+++ script.js
@@ -0,0 +1,3 @@
+fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=d53aae992f3042908c750d0f0e035e92')
+.then(response => response.json())
+.then(ListaNoticias => {console.log(ListaNoticias)})
