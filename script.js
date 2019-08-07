1aa6596d20e10113610c273b2afc179ae53bef57
NataliaLira <natalia.rlira@gmail.com>
2019-08-07 17:33:46 -0300

    using bootstrap material design

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=d53aae992f3042908c750d0f0e035e92')
-.then(response => response.json())
-.then(ListaNoticias => {console.log(ListaNoticias)})
+let button = document.querySelector('button');
+
+button.onclick = ()=> {
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=d53aae992f3042908c750d0f0e035e92')
+    .then(response => response.json())
+    .then(ListaNoticias => {console.log(ListaNoticias)})
+}
